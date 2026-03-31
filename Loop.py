a21fc358a3cec8ba14fdab907e5f3aedb4b5652b
Takee Dev <peerapolkoeram@gmail.com>
2026-03-31 22:34:57 +0700

    loop continue and break

---
+++ Loop.py
@@ -0,0 +1,41 @@
+number = [1,2,3,4,5,6,7]
+## continue
+for n in number:
+    if n == 3:
+         continue
+    print(n)
+
+print("-----------------")
+## break
+for n in number:
+    if n == 3:
+        break
+    print(n)
+
+
+
+
+
+users = [
+    {"name": "A", "age": 20},
+    {"name": "B", "age": None},
+    {"name": "C", "age": 30}
+]
+for u in users:
+    if u.get("age") is None:
+        continue
+    if u.get("age") >= 25:
+        print(u.get("name"))
+
+
+
+
+userBreak = [
+    {"name": "A", "age": 20},
+    {"name": "B", "age": 30},
+    {"name": "C", "age": 40}
+]
+for u in userBreak:
+    if u["age"] > 25:
+        print(u["name"])
+        break
