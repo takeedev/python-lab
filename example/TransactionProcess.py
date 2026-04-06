8d187b9a6f93a72db52971f8cfc2d98ea0788270
Takee Dev <peerapolkoeram@gmail.com>
2026-04-06 23:50:46 +0700

    update example

--- example/TransactionProcess.py
+++ example/TransactionProcess.py
@@ -1,4 +1,4 @@
-transactions = [
+transactions : list = [
     {"id": "T1", "user": "A", "amount": 100, "type": "credit"},
     {"id": "T2", "user": "A", "amount": 50, "type": "debit"},
     {"id": "T3", "user": "B", "amount": 200, "type": "credit"},
@@ -7,13 +7,37 @@ transactions = [
     {"id": "T6", "user": "C", "amount": 300, "type": "credit"},
 ]
 
-
-
-
 for t in transactions:
     if t.get("amount") is None:
         continue
     elif t.get("amount") < 0:
         print("invalid transaction:", {t.get("id")})
-    
 
+def process_transaction(user):
+    balances : int = 0
+    for r in transactions:
+        if r.get("user") == user:
+            if r.get("amount") is None or r.get("amount") < 0:
+                continue 
+            elif r.get("type") == "credit":
+                balances += r.get("amount") 
+            else:
+                balances -= r.get("amount")
+    return balances
+    
+user = []
+for e in transactions: 
+    user.append(e.get("user"))
+
+distinct = list(dict.fromkeys(user))
+
+
+output = []
+for d in distinct:
+    amount = process_transaction(d)
+    output.append((d, amount))
+
+print("User Balances:", output)
+
+for user, balance in output:
+    print(f"User: {user}, Balance: {balance}")
