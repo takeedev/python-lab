aa00d68af9bb5885a06b9590f76f1812d4f9140d
Takee Dev <peerapolkoeram@gmail.com>
2026-04-18 22:24:34 +0700

    array filtering

--- example/numpyTest.py
+++ example/numpyTest.py
@@ -32,3 +32,8 @@ print(arr.std())
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 print(a + b) 
+
+## array filtering
+arr1 = np.array([10, 20, 30, 40])
+result = arr[(arr > 10) & (arr < 40)]
+print(result)
